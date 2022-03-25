7db87720289c6f63ff89a8f763015c037286afee
ade <adeyds@gmail.com>
2022-03-25 15:28:34 +0700

    just camerax compose

--- app/src/main/java/me/pengj/arcompose/Camera.kt
+++ app/src/main/java/me/pengj/arcompose/Camera.kt
@@ -1,60 +1,116 @@
 package me.pengj.arcompose
 
+//import coil.compose.rememberImagePainter
 import android.annotation.SuppressLint
-import android.graphics.*
+import android.content.Context
+import android.net.Uri
 import android.util.Size
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
-import androidx.palette.graphics.Palette
-import java.io.ByteArrayOutputStream
+import coil.compose.rememberImagePainter
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.launch
 import java.util.concurrent.Executor
 
+@SuppressLint("CoroutineCreationDuringComposition")
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun SimpleCameraPreview(analyzer: ImageAnalysis.Analyzer) {
+fun SimpleCameraPreview(
+    cameraClick: (ImageCapture, Executor) -> Unit,
+    image: Uri? = null
+) {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
     val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    val imageCapture = remember { setupImageCapture() }
+    val executor = remember { ContextCompat.getMainExecutor(context) }
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val coroutine = rememberCoroutineScope()
 
-    AndroidView(
-        factory = { ctx ->
-            val preview = PreviewView(ctx)
-            val executor = ContextCompat.getMainExecutor(ctx)
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
-                bindPreview(
-                    lifecycleOwner,
-                    preview,
-                    cameraProvider,
-                    analyzer,
-                    executor
-                )
-            }, executor)
-            preview
-        },
+    coroutine.launch {
+        if (image == null) {
+            sheetState.hide()
+        } else {
+            sheetState.show()
+        }
+    }
+
+    ModalBottomSheetLayout(
         modifier = Modifier.fillMaxSize(),
+        content = mainContent(lifecycleOwner, context, cameraProviderFuture, imageCapture, executor, cameraClick),
+        sheetContent = sheetContent(image),
+        sheetState = sheetState,
+        sheetElevation = 50.dp,
+        sheetShape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
     )
 }
 
-@SuppressLint("UnsafeExperimentalUsageError")
+fun sheetContent(image: Uri?): @Composable() (ColumnScope.() -> Unit) = {
+    Column {
+        Image(painter = rememberImagePainter(image), contentDescription = null)
+    }
+}
+
+fun mainContent(
+    lifecycleOwner: LifecycleOwner,
+    context: Context,
+    cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
+    imageCapture: ImageCapture,
+    executor: Executor, cameraClick: (ImageCapture, Executor) -> Unit
+): @Composable () -> Unit = {
+    Box(contentAlignment = Alignment.BottomCenter) {
+        AndroidView(
+            factory = { ctx ->
+                val preview = PreviewView(ctx)
+                cameraProviderFuture.addListener({
+                    val cameraProvider = cameraProviderFuture.get()
+                    bindPreview(
+                        lifecycleOwner,
+                        preview,
+                        cameraProvider,
+                        imageCapture
+                    )
+                }, executor)
+                preview
+            },
+            modifier = Modifier.fillMaxSize(),
+        )
+        Button(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            onClick = {
+                cameraClick(imageCapture, executor)
+            },
+            content = {
+                Text(text = "ini button")
+            }
+        )
+    }
+}
+
 private fun bindPreview(
     lifecycleOwner: LifecycleOwner,
     previewView: PreviewView,
     cameraProvider: ProcessCameraProvider,
-    analyzer: ImageAnalysis.Analyzer,
-    executor: Executor
+    imageCapture: ImageCapture,
 ) {
     val preview = Preview.Builder().build().also {
         it.setSurfaceProvider(previewView.surfaceProvider)
@@ -68,18 +124,21 @@ private fun bindPreview(
     cameraProvider.bindToLifecycle(
         lifecycleOwner,
         cameraSelector,
-        setupImageAnalysis(executor, analyzer),
+        setupImageAnalysis(),
+        imageCapture,
         preview
     )
 }
 
-private fun setupImageAnalysis(executor: Executor, analyzer: ImageAnalysis.Analyzer): ImageAnalysis {
+private fun setupImageAnalysis(): ImageAnalysis {
     return ImageAnalysis.Builder()
         .setTargetResolution(Size(720, 1280))
         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
         .build()
-        .apply {
-            setAnalyzer(executor,analyzer)
-        }
 }
 
+private fun setupImageCapture(): ImageCapture {
+    return ImageCapture.Builder()
+        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+        .build()
+}
--- app/src/main/java/me/pengj/arcompose/MainActivity.kt
+++ app/src/main/java/me/pengj/arcompose/MainActivity.kt
@@ -1,21 +1,41 @@
 package me.pengj.arcompose
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.media.MediaScannerConnection
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.net.toFile
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import me.pengj.arcompose.ui.theme.ARComposeTheme
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
-    private val ViewModel by viewModels<ColorViewModel>()
+
+    private lateinit var outputDirectory: File
+    private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+    private var _image = MutableLiveData<Uri>()
+    private val image: LiveData<Uri>
+        get() = _image
+
+    /** Blocking camera operations are performed using this executor */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,9 +77,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun setViewContent() {
         setContent {
+            val image = image.observeAsState().value
             ARComposeTheme {
-                SimpleCameraPreview(PaletteAnalyzer { color -> ViewModel.setCurrentColorInt(color) })
-                DynamicPointMesh(Modifier.fillMaxSize(), ViewModel.currentColor)
+                SimpleCameraPreview(::cameraClick, image)
             }
         }
     }
@@ -70,10 +90,59 @@ class MainActivity : AppCompatActivity() {
         ) == PackageManager.PERMISSION_GRANTED
     }
 
+    private fun cameraClick(imageCapture: ImageCapture, cameraExecutor: Executor) {
+        // Create output file to hold the image
+        outputDirectory = getOutputDirectory(this)
+        val photoFile = createFile(outputDirectory, FILENAME, PHOTO_EXTENSION)
+
+        // Setup image capture metadata
+        val metadata = ImageCapture.Metadata().apply {
+
+            // Mirror image when using the front camera
+            isReversedHorizontal = lensFacing == CameraSelector.LENS_FACING_FRONT
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
+            .setMetadata(metadata)
+            .build()
+
+        // Setup image capture listener which is triggered after photo has been taken
+        imageCapture.takePicture(
+            outputOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
+                    Log.d(TAG, "Photo capture succeeded: $savedUri")
+
+                    val mimeType = MimeTypeMap.getSingleton()
+                        .getMimeTypeFromExtension(savedUri.toFile().extension)
+                    MediaScannerConnection.scanFile(
+                        this@MainActivity,
+                        arrayOf(savedUri.toFile().absolutePath),
+                        arrayOf(mimeType)
+                    ) { _, uri ->
+                        Log.d(TAG, "Image capture scanned into media store: $uri")
+                        _image.postValue(uri)
+                    }
+                }
+            })
+    }
+
     companion object {
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val PHOTO_EXTENSION = ".jpg"
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+
         const val FLAGS_FULLSCREEN =
             View.SYSTEM_UI_FLAG_LOW_PROFILE or
                     View.SYSTEM_UI_FLAG_FULLSCREEN or
@@ -81,5 +150,20 @@ class MainActivity : AppCompatActivity() {
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+        private fun createFile(baseFolder: File, format: String, extension: String) =
+            File(
+                baseFolder, SimpleDateFormat(format, Locale.US)
+                    .format(System.currentTimeMillis()) + extension
+            )
+
+        fun getOutputDirectory(context: Context): File {
+            val appContext = context.applicationContext
+            val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+                File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() }
+            }
+            return if (mediaDir != null && mediaDir.exists())
+                mediaDir else appContext.filesDir
+        }
     }
 }
